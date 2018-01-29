1ce79d13b74b6a357f3cee327056d734d7beb8cb
Herbert M. Turner <herbieturner@verizon.net>
2018-01-29 15:16:28 -0500

    Added structures in C

--- EDS/queues.py
+++
@@ -1,7 +0,0 @@
-class Queue:
-    def __init__(self,item_list=[]):
-        self.items = item_list
-    def enqueue(self, item):
-        self.items.append(item)
-    def dequeue(self):
-        return self.items.pop(self.items[0])
