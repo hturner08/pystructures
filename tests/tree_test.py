edd1db7c6c4a8278f44931856fd1f5379d55333c
Herbert M. Turner <herbieturner@verizon.net>
2018-02-11 22:29:36 -0500

    Build attempt 7

--- tests/tree_test.py
+++ tests/tree_test.py
@@ -3,12 +3,12 @@ import pytest
 
 class BinaryTreeTest(unittest.TestCase):
     def initialize(self):
-        self.node_one = new Node(5)
-        self.node_two = new Node(4)
-        self.node_three = new Node(2)
-        self.node_four = new Node(10)
-        self.node_five = new Node(-5)
-        self.node_six = new Node(20)
+        self.node_one = new Node(content = 5)
+        self.node_two = new Node(content = 4)
+        self.node_three = new Node(content = 2)
+        self.node_four = new Node(content = 10)
+        self.node_five = new Node(content = -5)
+        self.node_six = new Node(content = 20)
         self.tree_one = new Tree(node_one)
     def test_methods(self):
         self.assertTrue(self.tree_one.add_Node(node_four))
