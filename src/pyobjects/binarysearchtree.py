cc0baec5f6e02fd8f7e035849ca7d9fc9722aa6c
Herbert M. Turner <herbieturner@verizon.net>
2018-02-18 17:27:10 -0500

    Fixed syntax errors

--- src/pyobjects/binarysearchtree.py
+++ src/pyobjects/binarysearchtree.py
@@ -9,13 +9,13 @@ class BST_Node(Node):
         if self.right is None and node.compare_To(self) == 1:
             self.right = node
 
-        else if self.right is not None and node.compare_To(self) == 1:
+        elif self.right is not None and node.compare_To(self) == 1:
             self.right.add_Node(node)
 
-        else if self.left is None and node.compare_To(self) == -1:
+        elif self.left is None and node.compare_To(self) == -1:
             self.left = node
 
-        else if self.root.left is not None and node.compare_To(self) == -1:
+        elif self.root.left is not None and node.compare_To(self) == -1:
             self.left.add_Node(node)
 
         else:
@@ -28,21 +28,21 @@ class BST_Node(Node):
     def compare_To(self, other):
         if self.content==other.content:
             return 0
-        else if self.content > other.content:
+        elif self.content > other.content:
             return 1
         return -1
-    def updateFactor(self):
-        self.balanceFactor = height(self.left) − height(self.right)
+    # def updateFactor(self):
+    #     self.balanceFactor = height(self.left) − height(self.right)
 
 class BST_Tree(Tree):
     def add_Root(self, node):
-        if self.root == None
+        if self.root is None:
             self.root = node
         if node.compare_To(self.root) == 1:
             node.left = self.root
             self.root = node
             return True
-        else if node.compare_To(self.root) == -1:
+        elif node.compare_To(self.root) == -1:
             self.root = node.right
             self.root = node
             return True
@@ -51,13 +51,13 @@ class BST_Tree(Tree):
         if self.root.right is None and node.compare_To(self.root) == 1:
             self.root.right = node
 
-        else if self.root.right is not None and node.compare_To(self.root) == 1:
+        elif self.root.right is not None and node.compare_To(self.root) == 1:
             self.root.right.add_Node(node)
 
-        else if self.root.left is None and node.compare_To(self.root) == -1:
+        elif self.root.left is None and node.compare_To(self.root) == -1:
             self.root.left = node
 
-        else if self.root.left is not None and node.compare_To(self.root) == -1:
+        elif self.root.left is not None and node.compare_To(self.root) == -1:
             self.root.left.add_Node(node)
 
         else:
--- src/pyobjects/binarytree.py
+++ src/pyobjects/binarytree.py
@@ -1,74 +1,74 @@
 """Implements a basic binary tree: a tree where each node can have two children."""
+class Node:
+    def __init__(self, content=None, left = None, right = None):
+        self.left = left
+        self.right = right
+        self.content = content
+
+    def add_Content(self, content):
+        self.content = content
+
+    def add_left(self, node):
+        if self.left == None:
+            self.left = node
+            return true
+        else:
+            self.left.add_left(node)
+            return true
+
+    def add_right(self, node):
+        if self.right == None:
+            self.right = node
+            return true
+        else:
+            self.right.add_right(node)
+            return true
+    #Uses depth first traversal, adds right child of removed node to bottom of
+    #left child's tree
+    def remove_Node(self, node):
+        if self.left is node:
+            temp = self.left.right
+            self.left = self.left.left
+            self.left.add_left(self, node)
+            return True;
+        elif self.right is node:
+            tempt = self.right.right
+            self.right = self.right.left
+            self.right.add_left(self, node)
+            return True;
+        return False;
+    def contains_Depth(self, node):
+        if self.left is not None:
+            if self.left is node:
+                return True
+            self.left.contains_Depth(node)
+        if self.right is not None:
+            if self.right is node:
+                return True
+            self.right.contains_Depth(node)
+        return False
+
+    def contains_Breadth(self, node):
+        node_list = []
+        if self.left is not None:
+            if self.left is not node:
+                node_list.append(self.left)
+            else:
+                return True
+        if self.right is not None:
+            if self.right is not node:
+                node_list.append(self.right)
+            else:
+                return True
+        node_list.pop().contains_Breadth(node)
+
+    def height(self):
+        if self.left is not None:
+            return 1 + self.left.height()
+        if self.right is not None:
+            return 1 + self.left.height()
 
 class Tree:
-    class Node:
-        def __init__(self, content=None, left = None, right = None):
-            self.left = left
-            self.right = right
-            self.content = content
-
-        def add_Content(self, content):
-            self.content = content
-
-        def add_left(self, node):
-            if self.left == None:
-                self.left = node
-                return true
-            else:
-                self.left.add_left(node)
-                return true
-
-        def add_right(self, node):
-            if self.right == None:
-                self.right = node
-                return true
-            else:
-                self.right.add_right(node)
-                return true
-        #Uses depth first traversal, adds right child of removed node to bottom of
-        #left child's tree
-        def remove_Node(self, node):
-            if self.left is node:
-                temp = self.left.right
-                self.left = self.left.left
-                self.left.add_left(self, node)
-                return True;
-            else if self.right is node:
-                tempt = self.right.right
-                self.right = self.right.left
-                self.right.add_left(self, node)
-                return True;
-            return False;
-        def contains_Depth(self, node):
-            if self.left is not None
-                if self.left is node:
-                    return True
-                self.left.contains_Depth(node)
-            if self.right is not None:
-                if self.right is node:
-                    return True
-                self.right.contains_Depth(node)
-            return False
-
-        def contains_Breadth(self, node):
-            node_list = []
-            if self.left is not None:
-                if self.left is not node:
-                    node_list.append(self.left)
-                else:
-                    return True
-            if self.right is not None:
-                if self.right is not node:
-                    node_list.append(self.right)
-                else:
-                    return True
-            node_list.pop().contains_Breadth(node)
-
-        def height(self):
-            if self.left is not None:
-                return 1 + self.left.height()
-            if self.right is not None:
-                return 1 + self.left.height()
 
     def __init__(self, root = None):
         self.root = root
@@ -88,10 +88,10 @@ class Tree:
             if self.root.left == null:
                 self.root.left = node
                 return True
-            else if self.root.right == null:
+            elif self.root.right == null:
                 self.root.right = node
                 return True
-            else
+            else:
                 return self.root.left.add_left(node)
         return false
 
@@ -99,7 +99,7 @@ class Tree:
         return self.root.remove_Node(self, node)
 
     def contains_Depth(self, node):
-        if self.left is not None
+        if self.left is not None:
             if self.left is node:
                 return True
             self.left.contains_Depth(node)
